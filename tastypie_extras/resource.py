9dfb7e3b8bca30b0655980153ba196f545fa5193
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-02-20 22:00:47 +0100

    fixes and docstrings

--- tastypie_extras/resource.py
+++ tastypie_extras/resource.py
@@ -4,8 +4,16 @@ from tastypie.exceptions import NotFound
 from tastypie.utils.dict import dict_strip_unicode_keys
 
 
-class MultipartResource(object):
-    def deserialize(self, request, data, format=None):
+class MultipartResourceMixin(object):
+    """
+    Resource with upload image possibility
+    """
+    def deserialize(self, request, data, format=None):  # pylint: disable=I0011,W0622
+        """
+        Given a request, data and a format, deserializes the given data.
+
+        If content type is `multipart` then new behaviour, else old behaviour.
+        """
         content_type = format or request.META.get('CONTENT_TYPE', 'application/json')
 
         if content_type == 'application/x-www-form-urlencoded':
@@ -14,14 +22,18 @@ class MultipartResource(object):
             deserialized = request.POST.copy()
             deserialized.update(request.FILES)
         else:
-            deserialized = super(MultipartResource, self).deserialize(request, data, format)
+            deserialized = super(MultipartResourceMixin, self).deserialize(request, data, format)
 
         return deserialized
 
     def put_detail(self, request, **kwargs):
+        """
+        Either updates an existing resource or creates a new one with the
+        provided data.
+        """
         try:
             body = request.body
-        except Exception:
+        except Exception:  # pylint: disable=I0011,W0703
             body = None
         deserialized = self.deserialize(request, body, format=request.META.get('CONTENT_TYPE', 'application/json'))
         deserialized = self.alter_deserialized_detail_data(request, deserialized)
@@ -48,7 +60,14 @@ class MultipartResource(object):
                 return self.create_response(request, updated_bundle, response_class=http.HttpCreated, location=location)
 
     def post_list(self, request, **kwargs):
-        deserialized = self.deserialize(request, None, format=request.META.get('CONTENT_TYPE', 'application/json'))
+        """
+        Creates a new resource/object with the provided data.
+        """
+        try:
+            body = request.body
+        except Exception:  # pylint: disable=I0011,W0703
+            body = None
+        deserialized = self.deserialize(request, body, format=request.META.get('CONTENT_TYPE', 'application/json'))
         deserialized = self.alter_deserialized_detail_data(request, deserialized)
         bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
 
