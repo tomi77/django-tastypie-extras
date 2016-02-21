4c3a13e9a13f3d81c3e93dc39050566141c0ae94
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-02-21 09:35:08 +0100

    formatting

--- tastypie_extras/resource.py
+++ tastypie_extras/resource.py
@@ -1,3 +1,6 @@
+"""
+Tastypie resource mixins
+"""
 from django.core.exceptions import MultipleObjectsReturned
 from tastypie import http
 from tastypie.exceptions import NotFound
@@ -14,7 +17,8 @@ class MultipartResourceMixin(object):
 
         If content type is `multipart` then new behaviour, else old behaviour.
         """
-        content_type = format or request.META.get('CONTENT_TYPE', 'application/json')
+        content_type = format or request.META.get('CONTENT_TYPE',
+                                                  'application/json')
 
         if content_type == 'application/x-www-form-urlencoded':
             deserialized = request.POST
@@ -22,7 +26,8 @@ class MultipartResourceMixin(object):
             deserialized = request.POST.copy()
             deserialized.update(request.FILES)
         else:
-            deserialized = super(MultipartResourceMixin, self).deserialize(request, data, format)
+            deserialized = super(MultipartResourceMixin, self) \
+                .deserialize(request, data, format)
 
         return deserialized
 
@@ -35,29 +40,39 @@ class MultipartResourceMixin(object):
             body = request.body
         except Exception:  # pylint: disable=I0011,W0703
             body = None
-        deserialized = self.deserialize(request, body, format=request.META.get('CONTENT_TYPE', 'application/json'))
-        deserialized = self.alter_deserialized_detail_data(request, deserialized)
-        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
+        deserialized = self.deserialize(request, body,
+                                        format=request.META.get('CONTENT_TYPE',
+                                                                'application/json'))
+        deserialized = self.alter_deserialized_detail_data(request,
+                                                           deserialized)
+        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized),
+                                   request=request)
 
         try:
-            updated_bundle = self.obj_update(bundle=bundle, **self.remove_api_resource_names(kwargs))
+            updated_bundle = self.obj_update(bundle=bundle,
+                                             **self.remove_api_resource_names(kwargs))
 
             if not self._meta.always_return_data:
                 return http.HttpNoContent()
             else:
                 updated_bundle = self.full_dehydrate(updated_bundle)
-                updated_bundle = self.alter_detail_data_to_serialize(request, updated_bundle)
+                updated_bundle = self.alter_detail_data_to_serialize(request,
+                                                                     updated_bundle)
                 return self.create_response(request, updated_bundle)
         except (NotFound, MultipleObjectsReturned):
-            updated_bundle = self.obj_create(bundle=bundle, **self.remove_api_resource_names(kwargs))
+            updated_bundle = self.obj_create(bundle=bundle,
+                                             **self.remove_api_resource_names(kwargs))
             location = self.get_resource_uri(updated_bundle)
 
             if not self._meta.always_return_data:
                 return http.HttpCreated(location=location)
             else:
                 updated_bundle = self.full_dehydrate(updated_bundle)
-                updated_bundle = self.alter_detail_data_to_serialize(request, updated_bundle)
-                return self.create_response(request, updated_bundle, response_class=http.HttpCreated, location=location)
+                updated_bundle = self.alter_detail_data_to_serialize(request,
+                                                                     updated_bundle)
+                return self.create_response(request, updated_bundle,
+                                            response_class=http.HttpCreated,
+                                            location=location)
 
     def post_list(self, request, **kwargs):
         """
@@ -67,16 +82,24 @@ class MultipartResourceMixin(object):
             body = request.body
         except Exception:  # pylint: disable=I0011,W0703
             body = None
-        deserialized = self.deserialize(request, body, format=request.META.get('CONTENT_TYPE', 'application/json'))
-        deserialized = self.alter_deserialized_detail_data(request, deserialized)
-        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
+        deserialized = self.deserialize(request, body,
+                                        format=request.META.get('CONTENT_TYPE',
+                                                                'application/json'))
+        deserialized = self.alter_deserialized_detail_data(request,
+                                                           deserialized)
+        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized),
+                                   request=request)
 
-        updated_bundle = self.obj_create(bundle, **self.remove_api_resource_names(kwargs))
+        updated_bundle = self.obj_create(bundle,
+                                         **self.remove_api_resource_names(kwargs))
         location = self.get_resource_uri(updated_bundle)
 
         if not self._meta.always_return_data:
             return http.HttpCreated(location=location)
         else:
             updated_bundle = self.full_dehydrate(updated_bundle)
-            updated_bundle = self.alter_detail_data_to_serialize(request, updated_bundle)
-            return self.create_response(request, updated_bundle, response_class=http.HttpCreated, location=location)
+            updated_bundle = self.alter_detail_data_to_serialize(request,
+                                                                 updated_bundle)
+            return self.create_response(request, updated_bundle,
+                                        response_class=http.HttpCreated,
+                                        location=location)
